c511dacf525fc35401179d0f90bd39d69d47b0c1
wnagsong <wangsong1@bonc.com.cn>
2017-11-24 17:43:58 +0800

    更改结构

---
+++ front_end/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/components/app.js'
+
+ReactDOM.render(
+  <div>Hello World</div>,
+  document.getElementById('root')
+)
